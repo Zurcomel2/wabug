2ec7f7c6390beab33212bdaffe5f4e0cf202c0f2
Zurcomel2 <88980339+Zurcomel2@users.noreply.github.com>
2021-09-02 00:00:30 +0700

    Add files via upload

---
+++ menu/help.js
@@ -0,0 +1,170 @@
+/*  TQ TO JANGAN DI HAPUS, KALO NAMBAHIN NAMA LU GPP LAH*/
+
+const help = (prefix,shp,monoscape) => {
+	return `┏━━⬣  Virtex & Bug 🎐
+┃${monoscape}${shp} ${prefix}bugv1${monoscape}
+┃${monoscape}${shp} ${prefix}buglist${monoscape}
+┃${monoscape}${shp} ${prefix}vweb${monoscape}
+┃${monoscape}${shp} ${prefix}virgam${monoscape}
+┃${monoscape}${shp} ${prefix}virtag${monoscape}
+┃${monoscape}${shp} ${prefix}virtex${monoscape}
+┃${monoscape}${shp} ${prefix}bugsw${monoscape}
+┃${monoscape}${shp} ${prefix}bugpdf${monoscape}
+┃${monoscape}${shp} ${prefix}bugpc${monoscape}
+┃${monoscape}${shp} ${prefix}bugbutton${monoscape}
+┃${monoscape}${shp} ${prefix}bugkatalog${monoscape}
+┃${monoscape}${shp} ${prefix}bugimg${monoscape}
+┃${monoscape}${shp} ${prefix}buggif${monoscape}
+┃${monoscape}${shp} ${prefix}bugdoc${monoscape}
+┃${monoscape}${shp} ${prefix}bugtroli${monoscape}
+┃${monoscape}${shp} ${prefix}bugloc${monoscape}
+┃${monoscape}${shp} ${prefix}bugvn${monoscape}
+┃${monoscape}${shp} ${prefix}bugstik${monoscape}
+┃${monoscape}${shp} ${prefix}bugkontak${monoscape}
+┃${monoscape}${shp} ${prefix}buggc [ Jumlah ]${monoscape}
+┃${monoscape}${shp} ${prefix}sendbug${monoscape}
+┗━━⬣  ꪶ𖣂ꫂ 𝘛𝘚𝘜𝘒𝘈𝘚𝘈 ボ 𝘊𝘏𝘈𝘕 〽️
+
+┏━━⬣  All Menu 🎐
+┃${monoscape}${shp} 1.${prefix}add${monoscape} 
+┃${monoscape}${shp} 2.${prefix}kick${monoscape}
+┃${monoscape}${shp} 3.${prefix}promote${monoscape}
+┃${monoscape}${shp} 4.${prefix}demote${monoscape}
+┃${monoscape}${shp} 5.${prefix}sticker${monoscape}
+┃${monoscape}${shp} 6.${prefix}tahta${monoscape}
+┃${monoscape}${shp} 7.${prefix}glitch${monoscape}
+┃${monoscape}${shp} 8.${prefix}igstalk${monoscape}
+┃${monoscape}${shp} 9.${prefix}tovn${monoscape}
+┃${monoscape}${shp} 10.${prefix}wait${monoscape}
+┃${monoscape}${shp} 11.${prefix}linkgc${monoscape}
+┃${monoscape}${shp} 12.${prefix}leave${monoscape}
+┃${monoscape}${shp} 13.${prefix}toimg${monoscape}
+┃${monoscape}${shp} 14.${prefix}clearall${monoscape}
+┃${monoscape}${shp} 15.${prefix}bc${monoscape}
+┃${monoscape}${shp} 17.${prefix}img2url${monoscape}
+┃${monoscape}${shp} 18.${prefix}lirik${monoscape}
+┃${monoscape}${shp} 19.${prefix}hidetag${monoscape}
+┃${monoscape}${shp} 20.${prefix}meme${monoscape}
+┃${monoscape}${shp} 21.${prefix}info${monoscape}
+┃${monoscape}${shp} 22.${prefix}clone${monoscape}
+┃${monoscape}${shp} 23.${prefix}welcome${monoscape} 
+┃${monoscape}${shp} 25.${prefix}runtime${monoscape}
+┃${monoscape}${shp} 26.${prefix}upswteks${monoscape}
+┃${monoscape}${shp} 27.${prefix}upswimg${monoscape}
+┃${monoscape}${shp} 28.${prefix}upswvid${monoscape}
+┃${monoscape}${shp} 29.${prefix}spam [ Jumlah ]${monoscape}
+┃${monoscape}${shp} 30.${prefix}buatgc${monoscape}
+┃${monoscape}${shp} 31.${prefix}fastt${monoscape}
+┃${monoscape}${shp} 32.${prefix}slow${monoscape}
+┃${monoscape}${shp} 33.${prefix}reverse${monoscape}
+┃${monoscape}${shp} 34.${prefix}mirror${monoscape}
+┃${monoscape}${shp} 35.${prefix}hitamputih${monoscape}
+┃${monoscape}${shp} 36.${prefix}nosound${monoscape}
+┃${monoscape}${shp} 37.${prefix}gemes${monoscape}
+┃${monoscape}${shp} 38.${prefix}tempo${monoscape}
+┃${monoscape}${shp} 39.${prefix}nightcore${monoscape}
+┃${monoscape}${shp} 40.${prefix}fast${monoscape}
+┃${monoscape}${shp} 41.${prefix}tupai${monoscape}
+┃${monoscape}${shp} 42.${prefix}gemok${monoscape}
+┃${monoscape}${shp} 43.${prefix}bass${monoscape}
+┃${monoscape}${shp} 44.${prefix}budek${monoscape}
+┃${monoscape}${shp} 45.${prefix}ghost${monoscape}
+┃${monoscape}${shp} 46.${prefix}hode${monoscape}
+┃${monoscape}${shp} 47.${prefix}robot${monoscape}
+┃${monoscape}${shp} 48.${prefix}imut${monoscape}
+┃${monoscape}${shp} 49.${prefix}quoted${monoscape}
+┃${monoscape}${shp} 50.${prefix}golden${monoscape}
+┃${monoscape}${shp} 51.${prefix}wooden${monoscape}
+┃${monoscape}${shp} 52.${prefix}quote${monoscape}
+┃${monoscape}${shp} 53.${prefix}burn${monoscape}
+┃${monoscape}${shp} 54.${prefix}flower${monoscape}
+┃${monoscape}${shp} 55.${prefix}summer${monoscape}
+┃${monoscape}${shp} 56.${prefix}glow${monoscape}
+┃${monoscape}${shp} 57.${prefix}neon${monoscape}
+┃${monoscape}${shp} 58.${prefix}coffeecup2${monoscape}
+┃${monoscape}${shp} 59.${prefix}coffeecup${monoscape}
+┃${monoscape}${shp} 60.${prefix}battlefield${monoscape}
+┃${monoscape}${shp} 61.${prefix}google${monoscape}
+┃${monoscape}${shp} 62.${prefix}transformer${monoscape}
+┃${monoscape}${shp} 63.${prefix}maker3d4${monoscape}
+┃${monoscape}${shp} 64.${prefix}maker3d3${monoscape}
+┃${monoscape}${shp} 65.${prefix}maker3d${monoscape}
+┃${monoscape}${shp} 66.${prefix}maker3d2${monoscape}
+┃${monoscape}${shp} 67.${prefix}loli2${monoscape}
+┃${monoscape}${shp} 68.${prefix}maker2d4${monoscape}
+┃${monoscape}${shp} 69.${prefix}maker2d3${monoscape}
+┃${monoscape}${shp} 70.${prefix}maker2d2${monoscape}
+┃${monoscape}${shp} 71.${prefix}maker2d${monoscape}
+┃${monoscape}${shp} 72.${prefix}play${monoscape} 
+┃${monoscape}${shp} 73.${prefix}loli${monoscape}
+┃${monoscape}${shp} 74.${prefix}bukagrup${monoscape}
+┃${monoscape}${shp} 75.${prefix}tutupgrup${monoscape}
+┃${monoscape}${shp} 76.${prefix}upswlokasi${monoscape}
+┃${monoscape}${shp} 77.${prefix}upswaudio${monoscape}
+┃${monoscape}${shp} 78.${prefix}upswsticker${monoscape}
+┃${monoscape}${shp} 79.${prefix}nopref${monoscape}
+┃${monoscape}${shp} 80.${prefix}multi${monoscape}
+┃${monoscape}${shp} 81.${prefix}setprefix${monoscape}
+┃${monoscape}${shp} 82.${prefix}ngif${monoscape}
+┃${monoscape}${shp} 83.${prefix}hgif${monoscape}
+┃${monoscape}${shp} 84.${prefix}neko${monoscape}
+┃${monoscape}${shp} 85.${prefix}asupan${monoscape}
+┃${monoscape}${shp} 86.${prefix}join${monoscape}
+┃${monoscape}${shp} 86.${prefix}culik${monoscape}
+┃${monoscape}${shp} 87.${prefix}gasm${monoscape}
+┃${monoscape}${shp} 88.${prefix}eroyuri${monoscape}
+┃${monoscape}${shp} 89.${prefix}eroneko${monoscape}
+┃${monoscape}${shp} 90.${prefix}erokemonomimi${monoscape}
+┃${monoscape}${shp} 91.${prefix}erokitsune${monoscape}
+┃${monoscape}${shp} 92.${prefix}feet${monoscape}
+┃${monoscape}${shp} 93.${prefix}erofeet${monoscape}
+┃${monoscape}${shp} 94.${prefix}femdom${monoscape}
+┃${monoscape}${shp} 95.${prefix}futanari${monoscape}
+┃${monoscape}${shp} 96.${prefix}hentai${monoscape}
+┃${monoscape}${shp} 97.${prefix}holoero${monoscape}
+┃${monoscape}${shp} 98.${prefix}holo${monoscape}
+┃${monoscape}${shp} 99.${prefix}keta${monoscape}
+┃${monoscape}${shp} 100.${prefix}kitsune${monoscape}
+┃${monoscape}${shp} 101.${prefix}kemonomimi${monoscape}
+┃${monoscape}${shp} 102.${prefix}girlsolo${monoscape}
+┃${monoscape}${shp} 103.${prefix}tits${monoscape}
+┃${monoscape}${shp} 104.${prefix}trap${monoscape}
+┃${monoscape}${shp} 105.${prefix}yuri${monoscape}
+┃${monoscape}${shp} 106.${prefix}avatar${monoscape}
+┃${monoscape}${shp} 107.${prefix}gecg${monoscape}
+┃${monoscape}${shp} 108.${prefix}wallpaper${monoscape}
+┃${monoscape}${shp} 109.${prefix}foxgirl${monoscape}
+┃${monoscape}${shp} 110.${prefix}ytmp3${monoscape}
+┃${monoscape}${shp} 111.${prefix}ytmp4${monoscape}
+┃${monoscape}${shp} 112.${prefix}url2img${monoscape}
+┃${monoscape}${shp} 113.${prefix}ytsearch${monoscape}
+┃${monoscape}${shp} x ${monoscape}
+┃${monoscape}${shp} $ ${monoscape}
+┃${monoscape}${shp} > ${monoscape}
+┗━━⬣  ꪶ𖣂ꫂ 𝘛𝘚𝘜𝘒𝘈𝘚𝘈 ボ 𝘊𝘏𝘈𝘕 〽️
+
+┏━━⬣  Storage 🎐
+┃${monoscape}${shp} ${prefix}addimg${monoscape}
+┃${monoscape}${shp} ${prefix}listimg${monoscape}
+┃${monoscape}${shp} ${prefix}getimg${monoscape}
+┃${monoscape}${shp} ${prefix}addvn${monoscape}
+┃${monoscape}${shp} ${prefix}listvn${monoscape}
+┃${monoscape}${shp} ${prefix}getvn${monoscape}
+┃${monoscape}${shp} ${prefix}addstik${monoscape}
+┃${monoscape}${shp} ${prefix}liststik${monoscape}
+┃${monoscape}${shp} ${prefix}getstik${monoscape}
+┃${monoscape}${shp} ${prefix}addvid${monoscape}
+┃${monoscape}${shp} ${prefix}listvid${monoscape}
+┃${monoscape}${shp} ${prefix}getvid${monoscape}
+┗━━⬣  ꪶ𖣂ꫂ 𝘛𝘚𝘜𝘒𝘈𝘚𝘈 ボ 𝘊𝘏𝘈𝘕 〽️
+
+┏━━⬣  Thanks To 🎐
+┃ 🎐 MhankBarBar
+┃ 🎐 Diri Sendiri
+┃ 🎐 Orang Tua
+┃ 🎐 Tuhan Yang Maha Esa
+┃ 🎐 Dan Penyedia node_modules
+┗━━⬣  ꪶ𖣂ꫂ 𝘛𝘚𝘜𝘒𝘈𝘚𝘈 ボ 𝘊𝘏𝘈𝘕 〽️`
+}
+
+exports.help = help
